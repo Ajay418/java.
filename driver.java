18c30fd501dfae0b16b0c6321be9e9a92b63f559
Ajay418 <ajaymessi41@gmail.com>
2024-10-01 16:20:30 +0530

    Add files via upload

---
+++ driver.java
@@ -0,0 +1,35 @@
+import java.util.*;
+interface bank
+{
+    public void calculateInterest();
+}
+class SBI implements bank
+{
+    double r,amt,Interest;
+    public void calculateInterest()
+    {
+        r=0.7;amt=10000;
+        Interest=amt*r;
+        System.out.println("Interest="+Interest);
+    }
+}
+class Axis implements bank
+{
+    double r,amt,Interest;
+    public void calculateInterest()
+    {
+        r=0.2;amt=10000;
+        Interest=amt*r;
+        System.out.println("Interest="+Interest);
+    }
+}
+class Driver
+{
+    public void calculateInterest()
+    {
+        SBI sbi=new SBI();
+        sbi.calculateInterest();
+        Axis a=new Axis();
+        a.calculateInterest();
+    }
+}
